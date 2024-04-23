6594a8b2b07b29e18411053fae9cbf95dc777d05
Valentin Mazabraud <valentin@MacBook-Pro-de-Valentin.local>
2024-04-23 16:57:45 +0200

    ajout de la boucle

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import requests
 from flask import Flask
 from pymongo import MongoClient
+import threading
 
 app = Flask(__name__)
 client = MongoClient('172.20.0.2', 27017)
@@ -26,55 +27,63 @@ def get_weather_data(lat, lon, api_key):
         return None
 
 def get_data(lat, lon):
-    api_key = "430264947903dc36f471c78593348961" 
+    api_key = "74fba207e3c6e53a8e1b95c9457311a8" 
     return get_weather_data(lat, lon, api_key)
 
 def store_weather_data_in_db():
-    while True:
-        if 'city' not in db.list_collection_names():
-            city_collection = db['city']
-            with open('city.json', 'r', encoding='utf-8') as d:
-                data = json.load(d)
-                for city in data[:5]:  # Import only the first 5 cities
-                    city_collection.insert_one(city)
-
-        weather_collection = db['weather']
+    if 'city' not in db.list_collection_names():
         city_collection = db['city']
-        cursor = city_collection.find()
+        with open('city.json', 'r', encoding='utf-8') as d:
+            data = json.load(d)
+            for city in data[:5]:  # Import only the first 5 cities
+                city_collection.insert_one(city)
 
-        for document in cursor:
-            coord = document.get('coord')
-            if coord:
-                lat = coord.get('lat')
-                lon = coord.get('lon')
-                if lat is not None and lon is not None:
-                    weather_data = get_data(lat, lon)
-                    print('data', weather_data)
-                    if weather_data:
-                        weather_record = {
-                            "city_id": document['_id'],
-                            "name": document['name'],
-                            "coord": document['coord'],
-                            "temp_min": weather_data['main']['temp_min'],
-                            "temp_max": weather_data['main']['temp_max'],
-                            "humidity": weather_data['main']['humidity'],
-                            "pressure": weather_data['main']['pressure'],
-                            "wind_speed": weather_data['wind']['speed'],
-                            "insertion_time": datetime.now()
-                        }
-                        weather_collection.insert_one(weather_record)
-                        print(f"{document['name']} - Succès dans la collecte de données")
-                    else:
-                        print(f"Impossible de récupérer les données météorologiques pour {document['name']}.")
+    weather_collection = db['weather']
+    city_collection = db['city']
+    cursor = city_collection.find()
+
+    for document in cursor:
+        coord = document.get('coord')
+        if coord:
+            lat = coord.get('lat')
+            lon = coord.get('lon')
+            if lat is not None and lon is not None:
+                weather_data = get_data(lat, lon)
+                print('data', weather_data)
+                if weather_data:
+                    weather_record = {
+                        "city_id": document['_id'],
+                        "name": document['name'],
+                        "coord": document['coord'],
+                        "temp_min": weather_data['main']['temp_min'],
+                        "temp_max": weather_data['main']['temp_max'],
+                        "humidity": weather_data['main']['humidity'],
+                        "pressure": weather_data['main']['pressure'],
+                        "wind_speed": weather_data['wind']['speed'],
+                        "insertion_time": datetime.now()
+                    }
+                    weather_collection.insert_one(weather_record)
+                    print(f"{document['name']} - Succès dans la collecte de données")
+                    time.sleep(1)
                 else:
-                    print(f"Le document {document['_id']} ne contient pas de coordonnées valides.")
+                    print(f"Impossible de récupérer les données météorologiques pour {document['name']}.")
             else:
-                print(f"Le document {document['_id']} ne contient pas de champ 'coord'.")
-        time.sleep(300)
+                print(f"Le document {document['_id']} ne contient pas de coordonnées valides.")
+        else:
+            print(f"Le document {document['_id']} ne contient pas de champ 'coord'.")
 
 from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
-    store_weather_data_in_db()
-    
+    # Utilisation d'un thread afin de pouvoir faire un appel toutes les minutes a l'API
+    def run_flask():
+        app.run(debug=True, host='0.0.0.0', port=8080)
+
+    # Démarrer Flask dans un thread
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+
+    # Boucle pour appeler toute les minutes
+    while True:
+        store_weather_data_in_db()
+        time.sleep(60)
