4215d034d9b0d58878bf46f49ed9a440e6c98046
Valentin Mazabraud <valentin@MacBook-Pro-de-Valentin.local>
2024-04-23 15:56:28 +0200

    boucle recup data

--- api/app.py
+++ api/app.py
@@ -30,46 +30,47 @@ def get_data(lat, lon):
     return get_weather_data(lat, lon, api_key)
 
 def store_weather_data_in_db():
-    if 'city' not in db.list_collection_names():
+    while True:
+        if 'city' not in db.list_collection_names():
+            city_collection = db['city']
+            with open('city.json', 'r', encoding='utf-8') as d:
+                data = json.load(d)
+                for city in data[:5]:  # Import only the first 5 cities
+                    city_collection.insert_one(city)
+
+        weather_collection = db['weather']
         city_collection = db['city']
-        with open('city.json', 'r', encoding='utf-8') as d:
-            data = json.load(d)
-            for city in data[:5]:  # Import only the first 5 cities
-                city_collection.insert_one(city)
+        cursor = city_collection.find()
 
-    weather_collection = db['weather']
-    city_collection = db['city']
-    cursor = city_collection.find()
-
-    for document in cursor:
-        coord = document.get('coord')
-        if coord:
-            lat = coord.get('lat')
-            lon = coord.get('lon')
-            if lat is not None and lon is not None:
-                weather_data = get_data(lat, lon)
-                print('data', weather_data)
-                if weather_data:
-                    weather_record = {
-                        "city_id": document['_id'],
-                        "name": document['name'],
-                        "coord": document['coord'],
-                        "temp_min": weather_data['main']['temp_min'],
-                        "temp_max": weather_data['main']['temp_max'],
-                        "humidity": weather_data['main']['humidity'],
-                        "pressure": weather_data['main']['pressure'],
-                        "wind_speed": weather_data['wind']['speed'],
-                        "insertion_time": datetime.now()
-                    }
-                    weather_collection.insert_one(weather_record)
-                    print(f"{document['name']} - Succès dans la collecte de données")
-                    time.sleep(1)
+        for document in cursor:
+            coord = document.get('coord')
+            if coord:
+                lat = coord.get('lat')
+                lon = coord.get('lon')
+                if lat is not None and lon is not None:
+                    weather_data = get_data(lat, lon)
+                    print('data', weather_data)
+                    if weather_data:
+                        weather_record = {
+                            "city_id": document['_id'],
+                            "name": document['name'],
+                            "coord": document['coord'],
+                            "temp_min": weather_data['main']['temp_min'],
+                            "temp_max": weather_data['main']['temp_max'],
+                            "humidity": weather_data['main']['humidity'],
+                            "pressure": weather_data['main']['pressure'],
+                            "wind_speed": weather_data['wind']['speed'],
+                            "insertion_time": datetime.now()
+                        }
+                        weather_collection.insert_one(weather_record)
+                        print(f"{document['name']} - Succès dans la collecte de données")
+                    else:
+                        print(f"Impossible de récupérer les données météorologiques pour {document['name']}.")
                 else:
-                    print(f"Impossible de récupérer les données météorologiques pour {document['name']}.")
+                    print(f"Le document {document['_id']} ne contient pas de coordonnées valides.")
             else:
-                print(f"Le document {document['_id']} ne contient pas de coordonnées valides.")
-        else:
-            print(f"Le document {document['_id']} ne contient pas de champ 'coord'.")
+                print(f"Le document {document['_id']} ne contient pas de champ 'coord'.")
+        time.sleep(60)  # Pause d'une minute avant la prochaine itération
 
 from routes import *
 
