e30dfd82f0246084fad78792eab3c847a8308abc
Valentin Mazabraud <valentin@MacBook-Pro-de-Valentin.local>
2024-04-23 09:42:19 +0200

    Ajout commentaire

--- routes.py
+++ routes.py
@@ -1,6 +1,15 @@
 from flask import jsonify, request
 from app import app, db
 
+
+"""
+    GET: Retourne toutes les données météorologiques de la base de données.
+    Aucun paramètre n'est requis.
+
+    Réponse:
+        - Succès: Liste de toutes les entrées météorologiques avec ID, ID de la ville, et heure d'insertion formatée.
+        - Échec: Objet JSON indiquant l'absence de données.
+"""
 @app.route('/api/weather/all', methods=['GET'])
 def get_all_weather():
     weather_data = list(db.weather.find())
@@ -10,6 +19,16 @@ def get_all_weather():
         data['insertion_time'] = data['insertion_time'].strftime('%Y-%m-%d %H:%M:%S')
     return jsonify(weather_data) if weather_data else jsonify({"error": "Aucune donnée météorologique disponible"})
 
+
+"""
+    GET: Retourne les données météorologiques où l'humidité est supérieure ou égale au paramètre 'humidity'.
+    Paramètres:
+        - humidity (int): Valeur d'humidité minimum (inclus).
+
+    Réponse:
+        - Succès: Liste des données météorologiques correspondantes.
+        - Échec: Objet JSON indiquant l'absence de données pour l'humidité spécifiée.
+"""
 @app.route('/api/weather/humidity', methods=['GET'])
 def get_weather_by_humidity():
     humidity = request.args.get('humidity', type=int)
@@ -21,6 +40,17 @@ def get_weather_by_humidity():
         data['insertion_time'] = data['insertion_time'].strftime('%Y-%m-%d %H:%M:%S')
     return jsonify(weather_data) if weather_data else jsonify({"error": "Aucune donnée météorologique disponible pour l'humidité spécifiée"})
 
+
+"""
+    GET: Retourne les données météorologiques dont la température se situe entre les valeurs 'temp_min' et 'temp_max'.
+    Paramètres:
+        - temp_min (float): Température minimale (inclusive).
+        - temp_max (float): Température maximale (inclusive).
+
+    Réponse:
+        - Succès: Liste des données météorologiques correspondantes.
+        - Échec: Objet JSON indiquant l'absence de données pour la plage de température spécifiée.
+"""
 @app.route('/api/weather', methods=['GET'])
 def get_weather_by_temp_range():
     temp_min = request.args.get('temp_min', type=float)
@@ -32,5 +62,3 @@ def get_weather_by_temp_range():
         data['city_id'] = str(data['city_id'])
         data['insertion_time'] = data['insertion_time'].strftime('%Y-%m-%d %H:%M:%S')
     return jsonify(weather_data) if weather_data else jsonify({"error": "Aucune donnée météorologique disponible pour la plage de température spécifiée"})
-
-# Ensure to import these routes in app.py
