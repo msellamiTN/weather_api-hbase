8cd427c4fe46974b49fe2619c291669d3d4a07a3
Valentin Mazabraud <valentin@MacBook-Pro-de-Valentin.local>
2024-04-23 09:30:51 +0200

    Séparation des routes

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import json
-from bson import ObjectId
-from bson import json_util  # Importez json_util pour gérer la sérialisation ObjectId``
-from flask import Flask, jsonify, request
-from datetime import datetime
 import time
+from datetime import datetime
 import requests
-import pymongo
+from flask import Flask
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -23,134 +20,46 @@ def get_weather_data(lat, lon, api_key):
     try:
         response = requests.get(base_url, params=params)
         response.raise_for_status()
-        data = response.json()
-        return data
+        return response.json()
     except requests.exceptions.RequestException as e:
         print("Erreur de connexion:", e)
         return None
 
 def get_data(lat, lon):
     api_key = "74fba207e3c6e53a8e1b95c9457311a8" 
-    weather_data = get_weather_data(lat, lon, api_key)
-    if weather_data:
-         return weather_data
-    else:
-        print("Impossible de récupérer les prévisions météorologiques")
+    return get_weather_data(lat, lon, api_key)
 
 def store_weather_data_in_db():
     if 'city' not in db.list_collection_names():
         city_collection = db['city']
-        with open('city.json', encoding='utf-8') as d:
+        with open('city.json', 'r', encoding='utf-8') as d:
             data = json.load(d)
-            for index, city in enumerate(data):
-                if index >= 5:
-                    break  # Sortir de la boucle après avoir lu les 20 premières villes
-                lon = city['coord']['lon']
-                lat = city['coord']['lat']
-                name = city['name']
-                data = {
-                    "name": name,
-                    "coordinates": {"lon": lon, "lat": lat}
-                }
-                city_id = city_collection.insert_one(data).inserted_id
+            for city in data[:5]:  # Import only the first 5 cities
+                city_collection.insert_one(city)
 
-    if 'weather' not in db.list_collection_names():
-        W_collection = db['weather']
-
-    W_collection = db['weather']
+    weather_collection = db['weather']
     city_collection = db['city']
     cursor = city_collection.find()
 
     for document in cursor:
-        name = document['name']
-        id = document['_id']
-        lon = document['coordinates']['lon']
-        lat = document['coordinates']['lat']
-        rep = get_data(lat, lon)
-        temp_min = rep['main']['temp_min']
-        temp_max = rep['main']['temp_max']
-        humidity = rep['main']['humidity']
-        pressure = rep['main']['pressure']
-        wind_speed = rep['wind']['speed']
-        current_time = datetime.now()
+        weather_data = get_data(document['coordinates']['lat'], document['coordinates']['lon'])
+        if weather_data:
+            weather_record = {
+                "city_id": document['_id'],
+                "name": document['name'],
+                "coordinates": document['coordinates'],
+                "temp_min": weather_data['main']['temp_min'],
+                "temp_max": weather_data['main']['temp_max'],
+                "humidity": weather_data['main']['humidity'],
+                "pressure": weather_data['main']['pressure'],
+                "wind_speed": weather_data['wind']['speed'],
+                "insertion_time": datetime.now()
+            }
+            weather_collection.insert_one(weather_record)
+            print(f"{document['name']} - Succès dans la collecte de données")
+            time.sleep(1)
 
-        data_w = {
-            "city_id": id,
-            "name": name,
-            "coordinates": {"lon": lon, "lat": lat},
-            "temp_min": temp_min,
-            "temp_max": temp_max,
-            "humidity": humidity,
-            "pressure": pressure,
-            "wind_speed": wind_speed,
-            "insertion_time": current_time  
-        }
-
-        data_weather = W_collection.insert_one(data_w)
-
-        print(f"{name} - Succès dans la collecte de données")
-        
-        time.sleep(1)
-
-
-    
-@app.route('/api/weather/all', methods=['GET'])
-def get_all_weather():
-    weather_data = list(db.weather.find())
-    # Convertir les ObjectId en chaînes de caractères JSON
-    for data in weather_data:
-        data['_id'] = str(data['_id'])
-        data['city_id'] = str(data['city_id'])
-        # Convertir la date en un format sérialisable
-        data['insertion_time'] = data['insertion_time'].strftime('%Y-%m-%d %H:%M:%S')
-    if weather_data:
-        return jsonify(weather_data)
-    else:
-        return jsonify({"error": "Aucune donnée météorologique disponible"})
-    
-@app.route('/api/weather/humidity', methods=['GET'])
-def get_weather_by_humidity():
-    humidity = request.args.get('humidity')
-    
-    query = {
-        "humidity": {"$gte": int(humidity)}
-    }
-    weather_data = list(db.weather.find(query))
-    
-    # Convertir les ObjectId en chaînes de caractères JSON et formater l'heure d'insertion
-    for data in weather_data:
-        data['_id'] = str(data['_id'])
-        data['city_id'] = str(data['city_id'])
-        data['insertion_time'] = data['insertion_time'].strftime('%Y-%m-%d %H:%M:%S')
-    
-    if weather_data:
-        return jsonify(weather_data)
-    else:
-        return jsonify({"error": "Aucune donnée météorologique disponible pour l'humidité spécifiée"})
-
-
-
-@app.route('/api/weather', methods=['GET'])
-def get_weather_by_temp_range():
-    temp_min = request.args.get('temp_min')
-    temp_max = request.args.get('temp_max')
-    
-    query = {
-        "temp_min": {"$gte": float(temp_min)},
-        "temp_max": {"$lte": float(temp_max)}
-    }
-    weather_data = list(db.weather.find(query))
-    
-    # Convertir les ObjectId en chaînes de caractères JSON et formater l'heure d'insertion
-    for data in weather_data:
-        data['_id'] = str(data['_id'])
-        data['city_id'] = str(data['city_id'])
-        data['insertion_time'] = data['insertion_time'].strftime('%Y-%m-%d %H:%M:%S')
-    
-    if weather_data:
-        return jsonify(weather_data)
-    else:
-        return jsonify({"error": "Aucune donnée météorologique disponible pour la plage de température spécifiée"})
+from routes import *
 
 if __name__ == '__main__':
     store_weather_data_in_db()
